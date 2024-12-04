c8cb606b0b9cd8da5bf5e9c33b3963758e22b993
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2024-12-04 21:49:04 +0530

    feat: Day 3 part 2

--- 2024/3/main.go
+++ 2024/3/main.go
@@ -5,39 +5,60 @@ import (
 	"log"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	lines := internal.OpenInputFile("./input.txt").ReadLines()
+	line := strings.Join(internal.OpenInputFile("./input.txt").ReadLines(), "\n")
 
-	// sum of products of all numbers
+	// sum of products of all numbers, this time parse only returns enabled operations
 	total := 0
-	for _, line := range lines {
-		for _, num := range parse(line) {
-			total += num[0] * num[1]
-		}
+	for _, token := range parse(line) {
+		total += token.op1 * token.op2
 	}
 	log.Println("Final total is", total)
 }
 
-func parse(line string) [][]int {
-	mul_re := regexp.MustCompile(`mul\((?P<op1>\d+),(?P<op2>\d+)\)`)
-	matches := mul_re.FindAllStringSubmatch(line, -1)
-	op1_idx := mul_re.SubexpIndex("op1")
-	op2_idx := mul_re.SubexpIndex("op2")
+type token struct {
+	op1 int
+	op2 int
+}
 
-	// parge the numbers and multiply them
-	result := make([][]int, len(matches))
-	for i, match := range matches {
-		op1, err := strconv.ParseInt(match[op1_idx], 10, 32)
-		if err != nil {
-			log.Fatal("failed in parse", err)
+func parse(line string) []token {
+	f_re := regexp.MustCompile(`(mul\(\d+,\d+\))|((do|don\'t)\(\))`)
+	f_matches := f_re.FindAllString(line, -1)
+
+	enabled := true // condition for enabling future multiplications
+	result := []token{}
+	for _, f_match := range f_matches {
+		switch f_match {
+		case "do()":
+			enabled = true
+		case "don't()":
+			enabled = false
+		default:
+			if !enabled { // skip disabled operations
+				continue
+			}
+
+			re := regexp.MustCompile(`mul\((?P<op1>\d+),(?P<op2>\d+)\)`)
+			match := re.FindStringSubmatch(f_match)
+			op1_idx := re.SubexpIndex("op1")
+			op2_idx := re.SubexpIndex("op2")
+
+			op1, err := strconv.ParseInt(match[op1_idx], 10, 32)
+			if err != nil {
+				log.Fatal("failed in parse", err)
+			}
+			op2, err := strconv.ParseInt(match[op2_idx], 10, 32)
+			if err != nil {
+				log.Fatal("failed in parse", err)
+			}
+
+			// only append parsed results if followed by a do(), disable if followed by don't()
+			// thus, it only returns enabled operations
+			result = append(result, token{int(op1), int(op2)})
 		}
-		op2, err := strconv.ParseInt(match[op2_idx], 10, 32)
-		if err != nil {
-			log.Fatal("failed in parse", err)
-		}
-		result[i] = []int{int(op1), int(op2)}
 	}
 
 	return result
