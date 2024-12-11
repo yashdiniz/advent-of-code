16772fcbc25f1a2d669d8ad10bdc8a11ec062572
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2024-12-11 13:40:50 +0530

    feat: Day 6 part 1

--- 2024/6/main.go
+++ 2024/6/main.go
@@ -3,40 +3,33 @@ package main
 import (
 	"aoc2024/internal"
 	"log"
+	"slices"
 )
 
 func main() {
 	lines := internal.OpenInputFile("./test.txt").ReadLines()
 	board_sz_x, board_sz_y := len(lines[0]), len(lines)
-	var board []location // board is a sparse matrix of locations (x, y, obj)
+	board, g := getBoard(lines)
+	var visited []location
 
-	// parse the board
-	var g guard
-	for y, line := range lines {
-		for x, c := range line {
-			switch c {
-			case '#':
-				board = append(board, location{x, y, obstacle})
-			case '^':
-				g = guard{x, y, up}
-			case '>':
-				g = guard{x, y, right}
-			case '<':
-				g = guard{x, y, left}
-			case 'v':
-				g = guard{x, y, down}
-			}
+	for {
+		if !slices.Contains(visited, location{g.x, g.y, VISITED}) { // already visited this cell
+			visited = append(visited, location{g.x, g.y, VISITED})
+		}
+		g = g.NextPossibleMove(board)
+		if g.x < 0 || g.x >= board_sz_x || g.y < 0 || g.y >= board_sz_y { // out of bounds
+			break
 		}
 	}
-	log.Println(board_sz_x, board_sz_y, board)
-	log.Println("Guard:", g)
+
+	log.Println("Visited count:", len(visited), board_sz_x, board_sz_y)
 }
 
 type object rune
 
 const (
-	obstacle object = '#'
-	visited  object = 'X'
+	OBSTACLE object = '#'
+	VISITED  object = 'X'
 )
 
 type location struct {
@@ -47,10 +40,10 @@ type location struct {
 type direction rune
 
 const (
-	up    direction = '^'
-	down  direction = 'v'
-	left  direction = '<'
-	right direction = '>'
+	UP    direction = '^'
+	DOWN  direction = 'v'
+	LEFT  direction = '<'
+	RIGHT direction = '>'
 )
 
 func (d direction) String() string {
@@ -61,22 +54,73 @@ func (d direction) String() string {
 func (d direction) Turn() direction {
 	var res direction
 	switch d {
-	case up:
-		res = right
-	case right:
-		res = down
-	case down:
-		res = left
-	case left:
-		res = up
+	case UP:
+		res = RIGHT
+	case RIGHT:
+		res = DOWN
+	case DOWN:
+		res = LEFT
+	case LEFT:
+		res = UP
 	default:
 		return d
 	}
-	d = res
 	return res
 }
 
 type guard struct {
 	x, y int
-	dir  direction // '^><v'
+	dir  direction
+}
+
+func (g guard) NextPossibleMove(board []location) guard {
+	switch g.dir {
+	case UP:
+		if slices.Contains(board, location{g.x, g.y - 1, OBSTACLE}) {
+			g.dir = g.dir.Turn()
+		} else {
+			g.y--
+		}
+	case RIGHT:
+		if slices.Contains(board, location{g.x + 1, g.y, OBSTACLE}) {
+			g.dir = g.dir.Turn()
+		} else {
+			g.x++
+		}
+	case DOWN:
+		if slices.Contains(board, location{g.x, g.y + 1, OBSTACLE}) {
+			g.dir = g.dir.Turn()
+		} else {
+			g.y++
+		}
+	case LEFT:
+		if slices.Contains(board, location{g.x - 1, g.y, OBSTACLE}) {
+			g.dir = g.dir.Turn()
+		} else {
+			g.x--
+		}
+	}
+	return g
+}
+
+func getBoard(lines []string) ([]location, guard) {
+	var board []location // board is a sparse matrix of locations (x, y, obj)
+	var g guard
+	for y, line := range lines {
+		for x, c := range line {
+			switch c {
+			case '#':
+				board = append(board, location{x, y, OBSTACLE})
+			case '^':
+				g = guard{x, y, UP}
+			case '>':
+				g = guard{x, y, RIGHT}
+			case '<':
+				g = guard{x, y, LEFT}
+			case 'v':
+				g = guard{x, y, DOWN}
+			}
+		}
+	}
+	return board, g
 }
