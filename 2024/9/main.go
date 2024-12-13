3501e27359ad8d3ec939aab939574a72d4a7cb4e
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2024-12-13 11:40:25 +0530

    feat: Day 9 part 2

--- 2024/9/main.go
+++ 2024/9/main.go
@@ -11,7 +11,7 @@ func main() {
 	lines := internal.OpenInputFile("input.txt").ReadLines()
 	dense := lines[0]
 	fs := toBlocks(dense)
-	fs = compactBlocks(fs)
+	fs = defragBlocks(fs)
 	sum := checksum(fs)
 	log.Println("Checksum:", sum)
 }
@@ -45,6 +45,42 @@ func toBlocks(dense string) []Block {
 	return fs
 }
 
+func mergeAdjBlocks(fs []Block) []Block {
+	for i := 0; i < len(fs)-1; i++ {
+		if fs[i].id == fs[i+1].id {
+			fs[i].size += fs[i+1].size
+			fs = slices.Delete(fs, i+1, i+2)
+		}
+	}
+	for fs[len(fs)-1].id == -1 { // if the last blocks are space blocks, remove them
+		fs = slices.Delete(fs, len(fs)-1, len(fs))
+	}
+	return fs
+}
+
+func defragBlocks(fs []Block) []Block {
+	for id := fs[len(fs)-1].id; id >= 0; id-- {
+		mergeAdjBlocks(fs)
+		i := slices.IndexFunc(fs, func(b Block) bool { // find the first block with the given id
+			return b.id == id
+		})
+		spc_idx := slices.IndexFunc(fs, func(b Block) bool { // find the first space block
+			return b.id == -1 && fs[i].size <= b.size
+		})
+		if spc_idx > i || spc_idx == -1 { // skip if space block after the current block
+			continue
+		}
+		g := fs[spc_idx].size         // the size of the space block
+		fs[spc_idx].id = fs[i].id     // move the block, to fill the space
+		fs[spc_idx].size = fs[i].size // move the block, to fill the space
+		fs[i].id = -1                 // mark the current block as a space block
+		if g > fs[i].size {           // if there's still space left in the space block
+			fs = slices.Insert(fs, spc_idx+1, Block{-1, g - fs[i].size}) // insert a new space block to keep remaining space
+		}
+	}
+	return fs
+}
+
 func compactBlocks(fs []Block) []Block {
 	for {
 		i := len(fs) - 1
@@ -60,11 +96,6 @@ func compactBlocks(fs []Block) []Block {
 			break
 		}
 
-		// g := Block{fs[i].id, fs[i].size - fs[spc_idx].size}
-		// if g.size > 0 {
-		// 	fs = slices.Replace(fs, spc_idx, spc_idx+1, g)
-		// }
-		// TODO: fix this
 		if fs[spc_idx].size <= fs[i].size { // if the space block is the same size or smaller than the current block
 			fs[spc_idx].id = fs[i].id                  // move the block, to fill the space
 			fs[i].size = fs[i].size - fs[spc_idx].size // reduce the current block size
@@ -76,7 +107,7 @@ func compactBlocks(fs []Block) []Block {
 				Block{fs[i].id, fs[i].size},              // fill the space with the current block
 				Block{-1, fs[spc_idx].size - fs[i].size}, // and keep the remaining space
 			)
-			fs = slices.Delete(fs, len(fs)-1, len(fs)) // remove the last block, which is now moved
+			fs = slices.Delete(fs, i, i+1) // remove the last block, which is now moved
 		}
 	}
 	return fs
@@ -85,6 +116,10 @@ func compactBlocks(fs []Block) []Block {
 func checksum(fs []Block) int {
 	var sum, pos int
 	for _, b := range fs {
+		if b.id == -1 { // skip spaces
+			pos += b.size
+			continue
+		}
 		for i := 0; i < b.size; i++ {
 			sum += b.id * pos
 			pos++
