8254f1bad67ece3b45a1b22f442adcc577b8c5a7
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-12-01 15:02:38 +0530

    feat: aoc2025 Day 1 part 1 & part 2

---
+++ 2025/1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"aoc2025/internal"
+	"log"
+	"strconv"
+)
+
+func main() {
+	input := internal.OpenInputFile("./input.txt")
+	lines := input.ReadLines()
+
+	// safe starts at 50, need to count number of times it hits 0
+	cnt0, safeptr := 0, 50
+	for _, l := range lines {
+		dir := parse(l)
+		for i := 0; i < dir[1]; i++ {
+			if dir[0] < 0 {
+				safeptr--
+			} else {
+				safeptr++
+			}
+			if safeptr < 0 {
+				safeptr += 100
+			} else if safeptr >= 100 {
+				safeptr %= 100
+			}
+			if safeptr == 0 {
+				cnt0++
+			}
+		}
+	}
+	log.Println("Answer:", cnt0, safeptr)
+}
+
+func parse(line string) [2]int {
+	var out [2]int
+	if line[0] == 'L' {
+		out[0] = -1
+	} else {
+		out[0] = 1
+	}
+	v, err := strconv.ParseInt(line[1:], 10, 32)
+	if err != nil {
+		log.Fatal("failed in parse", err)
+	}
+	out[1] = int(v)
+	return out
+}
