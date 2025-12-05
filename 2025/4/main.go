a919a6e2dcf7eeefd82445b2a6df49434c07d66c
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-12-05 16:18:46 +0530

    feat: Day 4 part 2

--- 2025/4/main.go
+++ 2025/4/main.go
@@ -12,26 +12,36 @@ func main() {
 		{1, -1}, {1, 0}, {1, 1},
 	}
 	input := internal.OpenInputFile("./input.txt")
-	lines := input.ReadLines()
+	var lines [][]rune
+	for _, l := range input.ReadLines() {
+		lines = append(lines, []rune(l))
+	}
 
 	sum := 0
-	for i, line := range lines {
-		for j, roll := range line {
-			total := 0
-			if roll == '@' {
-				for _, d := range dirs {
-					x, y := i+d[0], j+d[1]
-					if x < 0 || x >= len(lines) || y < 0 || y >= len(line) {
-						continue
+	for {
+		tsum := sum
+		for i, line := range lines {
+			for j, roll := range line {
+				total := 0
+				if roll == '@' {
+					for _, d := range dirs {
+						x, y := i+d[0], j+d[1]
+						if x < 0 || x >= len(lines) || y < 0 || y >= len(line) {
+							continue
+						}
+						if lines[x][y] == '@' {
+							total++
+						}
 					}
-					if lines[x][y] == '@' {
-						total++
+					if total < 4 {
+						sum++
+						lines[i][j] = '.'
 					}
-				}
-				if total < 4 {
-					sum++
 				}
 			}
+		}
+		if tsum == sum {
+			break
 		}
 	}
 	log.Println("Answer:", sum)
