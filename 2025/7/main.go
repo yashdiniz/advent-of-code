9498f51186fff0169327a614c55e8d4541d0fc1e
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-12-10 17:56:10 +0530

    feat: Day 7 part 2 Backtracking, memoization

--- 2025/7/main.go
+++ 2025/7/main.go
@@ -18,28 +18,50 @@ func main() {
 		}
 	}
 
-	count := 0
-	for i, row := range grid {
-		for j, cell := range row {
-			switch cell {
-			case 'S':
-				grid[i+1][j] = '|'
-			case '^':
-				if grid[i-1][j] == '|' {
-					grid[i][j-1], grid[i][j+1] = '|', '|'
-					count++
-				}
-			case '.':
-				if i > 0 && grid[i-1][j] == '|' {
-					grid[i][j] = '|'
-				}
-			}
+	// find S
+	s_j := 0
+	for j, c := range grid[0] {
+		if c == 'S' {
+			s_j = j
 		}
 	}
 
-	// print grid
-	for _, r := range grid {
-		log.Print(string(r))
+	memo := make(map[[2]int]int)
+	log.Print("answer: ", worlds(grid, 0, s_j, memo))
+}
+
+func worlds(grid [][]rune, i, j int, memo map[[2]int]int) int {
+	if i == len(grid) {
+		return 1
 	}
-	log.Print("answer: ", count)
+	log.Println("Entered:", i, j, string(grid[i][j]), memo[[2]int{i, j}])
+	if v, ok := memo[[2]int{i, j}]; ok {
+		log.Println("memoized result:", i, j, v)
+		return v
+	}
+
+	cell := grid[i][j]
+	var tmp int
+	switch cell {
+	case 'S':
+		grid[i+1][j] = '|'
+		tmp = worlds(grid, i+1, j, memo)
+	case '^':
+		if grid[i-1][j] == '|' {
+			grid[i][j-1] = '|'
+			tmp = worlds(grid, i+1, j-1, memo)
+			grid[i][j+1] = '|'
+			tmp += worlds(grid, i+1, j+1, memo)
+		}
+	case '.':
+		if i > 0 && grid[i-1][j] == '|' {
+			grid[i][j] = '|'
+			tmp = worlds(grid, i+1, j, memo)
+		}
+	case '|':
+		tmp = worlds(grid, i+1, j, memo)
+	}
+
+	memo[[2]int{i, j}] = tmp
+	return tmp
 }
